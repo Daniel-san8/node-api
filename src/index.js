84f6bc453795fc9e29a1a9e506006706eec6b22f
Daniel San <danielgdt88@hotmail.com>
2025-03-26 22:28:52 -0300

    feat: urlparsed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const http = require("http");
-const routes = require("./routes")
-const server = http.createServer((request, response) => {
+const routes = require("./routes");
+const url = require("url");
 
-    console.log(`Request method: ${request.method} and Endpoint: ${request.url}`);
-    const route = routes.find(route => route.endpoint === request.url && route.method === request.method )
+const server = http.createServer((request, response) => {
+    const parsedUrl = url.parse(request.url, true);
+    console.log(`Request method: ${request.method} and Endpoint: ${parsedUrl.pathname}`);
+    const route = routes.find(route => route.endpoint === parsedUrl.pathname && route.method === request.method )
 
     if(route) {
+        request.query = parsedUrl.query;
         route.handler(request, response)
     } else {
         response.writeHead(404, { "Content-Type": "text/html" })
-        response.end("<h1>Error 404</h1>")
+        response.end("<h1>Error 404</h1>" + parsedUrl.pathname)
     }
     // if(request.url === "/users" && request.method === "GET") {
     //     controller.listUsers(request, response)
