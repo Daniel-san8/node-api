c182c68029742dd33e50ccc5d5ae461e56004529
Daniel San <danielgdt88@hotmail.com>
2025-03-26 21:38:53 -0300

    feat: crio regra de rotas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 const http = require("http");
+const users = require("./mocks/users")
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, { "content-type": "text/html" });
-    response.end("<h1>Hello world!</h1>");
+
+    console.log(`Request method: ${request.method} and Endpoint: ${request.url}`);
+
+    if(request.url === "/users" && request.method === "GET") {
+        response.writeHead(200, { "content-type": "application/json" });
+        response.end(JSON.stringify(users));
+    } else {
+        response.writeHead(404, { "content-type": "text/html" });
+        response.end("<h1>Error 404</h1>");
+    }
+
+    // response.writeHead(200, { "content-type": "text/html" });
+    // response.end("<h1>Hello!</h1>");
 })
 
 server.listen(3000, () => console.log("servidor de pé"));
---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: "Daniel San"
+    },
+    {
+        id: 2,
+        name: "Gabriel Souza"
+    },
+    {
+        id: 3,
+        name: "Fernandão"
+    }
+]
